9f0cbc05b69f1724f91caebce996175fbb448969
jacky254 <mwasjcindy@gmail.com>
2023-02-22 18:50:04 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * add - adds two integers
+ * @n1: integer 1
+ * @n2: integer 2
+ * Description: give two integers, returns sum
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
+
+
+100-times_table.c
+
+
+#include "main.h"
+
+/**
  * print_times_table - Prints a multiplication table up to param
  * @n: The number to be treated
  *
@@ -49,3 +72,4 @@ void print_times_table(int n)
 		}
 	}
 }
+
